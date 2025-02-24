989b7698ac1d5463c4d2383bdf9beace63b1c2a4
tae4an <xotks7524@gmail.com>
2025-02-24 19:25:20 +0900

    feat: 앱 초기화 및 다중 프로바이더 설정 추가, 다국어 지원 및 라우팅 개선

--- driver_app/lib/main.dart
+++ driver_app/lib/main.dart
@@ -1,122 +1,77 @@
-import 'package:flutter/material.dart';
+// 앱의 진입점 파일
+// Flutter 앱 초기화, 프로바이더 설정, 테마 적용을 담당
 
-void main() {
+import 'package:driver_app/config/app_config.dart';
+import 'package:driver_app/core/location/location_service.dart';
+import 'package:driver_app/presentation/providers/auth_provider.dart';
+import 'package:driver_app/presentation/providers/connection_provider.dart';
+import 'package:driver_app/presentation/providers/driver_provider.dart';
+import 'package:driver_app/presentation/themes/app_theme.dart';
+import 'package:driver_app/routes/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  // Flutter 엔진 초기화 및 위젯 바인딩 보장
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // 앱 환경 설정 로드
+  await AppConfig.initialize();
+  
+  // 앱 실행
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    // 다중 프로바이더 설정으로 앱 전역 상태 관리
+    return MultiProvider(
+      providers: [
+        // 인증 상태 관리
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        
+        // 네트워크 연결 상태 관리
+        ChangeNotifierProvider(create: (_) => ConnectionProvider()),
+        
+        // 기사 상태 관리 (버스 정보, 운행 상태 등)
+        ChangeNotifierProvider(create: (_) => DriverProvider()),
+        
+        // 위치 서비스 프로바이더 (백그라운드 위치 추적)
+        Provider(create: (_) => LocationService()),
+      ],
+      child: Consumer<AuthProvider>(
+        builder: (context, authProvider, _) {
+          return MaterialApp(
+            title: '기사용 앱',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.system, // 시스템 테마 따르기
+            
+            // 라우팅 설정
+            initialRoute: authProvider.isLoggedIn ? AppRoutes.home : AppRoutes.login,
+            onGenerateRoute: AppRouter.generateRoute,
+            
+            // 다국어 지원 설정
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('ko', 'KR'), // 한국어
+              Locale('en', 'US'), // 영어
+            ],
+            locale: const Locale('ko', 'KR'), // 기본 언어
+            
+            // 디버그 배너 제거
+            debugShowCheckedModeBanner: false,
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
