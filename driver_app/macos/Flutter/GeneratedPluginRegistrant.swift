492d2ab4c953b07bf0e6d946eacac4eb4e163390
tae4an <xotks7524@gmail.com>
2025-03-11 21:57:22 +0900

    feat: Flutter 플러그인 등록 및 macOS Podfile.lock 추가

--- driver_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ driver_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import device_info_plus
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import geolocator_apple
+import location
+import path_provider_foundation
 import shared_preferences_foundation
+import sqflite_darwin
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
